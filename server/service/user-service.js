a6d1738c5329ca7e65539beb22557aa459cc4bf6
sagynzxc1 <124157773+sagynzxc1@users.noreply.github.com>
2023-08-25 18:10:43 +0600

    asdfg

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,5 +1,7 @@
-const UserSchema  = require('../models/user-model')
+const UserSchema = require('../models/user-model')
 const bcrypt = require('bcrypt')
+const mailService = require('./mail-service')
+const TokenService = require('./token-service')
 class UserService {
     async registration(email , password){
         const candidate = await_UserModel.findOne( {email})
@@ -7,7 +9,11 @@ class UserService {
             throw new Error(`Пользователь с почтовым адресом ${email} не существует!`)
 
         }
-        const hashPassword = await bcrypt.hash(password, 3)
-        const user = await UserModel.create({email, password: hashPassword})
+        const hashPassword = await bcrypt.hash(password, 3);
+        const activationLink = uuid.v4();
+        const user = await UserModel.create({email, password: hashPassword, activationLink});
+        await mailService.sendActivationMail(email, activationLink  )
+        const tokens = TokenService.generateTokens
     }
 }
+module.exports = new UserService();
